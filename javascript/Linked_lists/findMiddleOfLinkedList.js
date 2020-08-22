e98898c1d760605c4e4203b26b7fa670e9497a4a
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-22 12:53:25 +0100

    Solving the problem of finding the middle of a linked list using the two pointers

---
+++ javascript/Linked_lists/findMiddleOfLinkedList.js
@@ -0,0 +1,9 @@
+const middleNode = head => {
+    let fast = head
+    let slow = head
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+    }
+    return slow;
+};
