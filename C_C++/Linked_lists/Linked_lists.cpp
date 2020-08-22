ee3cfec623ed560d43c168afeecd22fff78f6508
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-22 13:42:14 +0100

    starting implementation in C++

--- C_C++/Linked_lists/Linked_lists.cpp
+++ C_C++/Linked_lists/Linked_lists.cpp
@@ -1,1 +1,40 @@
 #include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+};
+
+void    printList(Node *head)
+{
+    while (head != NULL)
+    {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int     main(void)
+{
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
+
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1;
+    head->next = second;
+    second->data = 13;
+    second->next = third;
+    third->data = 899;
+    third->next = NULL;
+
+    printList(head);
+
+    return (0);
+}
