cabd99e2bb49ec86361d8f7c358b452567a32b73
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-08-29 22:11:55 +0100

    Adding the implementation of the queue DS using arrays which is very fucking EZ

--- javascript/Queue/queue.js
+++ javascript/Queue/queue.js
@@ -1,1 +1,33 @@
 // this is the queue implementation in javascript
+class Queue {
+	constructor() {
+		this.items = [];
+	}
+	// Functions to be implemented
+	// enqueue(item)
+	// dequeue()
+	// front()
+	// isEmpty()
+	// printQueue()
+	Enqueue(element) {
+		this.items.push(element);
+	}
+	isEmpty() {
+		return this.items.length === 0;
+	}
+	Deque() {
+		if (isEmpty())
+			return "Underflow";
+		return this.items.shift();
+	}
+	front() {
+		if (isEmpty())
+			return "No Element in the Queue";
+		return this.items[0];
+	}
+	printQueue() {
+		let str = '';
+		for (let i = 0; i < this.items.length; i++)
+			str += this.items[i] + ' ';
+		return str;
+};
