e8ae195774d6930ab94b1b861b3c5b81ed55e725
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-09-04 00:05:01 +0100

    Starting the peak finding problem but I still need to complet it

---
+++ leetcodeProblems/162.find-peak-element.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=162 lang=javascript
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+	// let arr = [];
+	// for (let i = 0; i < nums.length - 1; i++)
+	// 	arr[i] = nums[i];
+	// nums = nums.sort();
+	// let res = arr.indexOf(nums[nums.length - 1])
+	// if (res === -1)
+	// 	return 0;
+	// return res;
+};
+// @lc code=end
+
+
+console.log(findPeakElement([1]));
