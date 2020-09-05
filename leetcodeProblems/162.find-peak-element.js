bf0ee5bef0c046aed81597a023c42637ce190d88
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-09-05 15:04:58 +0100

    Back to the problems

--- leetcodeProblems/162.find-peak-element.js
+++ leetcodeProblems/162.find-peak-element.js
@@ -10,14 +10,14 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-	// let arr = [];
-	// for (let i = 0; i < nums.length - 1; i++)
-	// 	arr[i] = nums[i];
-	// nums = nums.sort();
-	// let res = arr.indexOf(nums[nums.length - 1])
-	// if (res === -1)
-	// 	return 0;
-	// return res;
+	let left = 0;
+	let right = nums.length - 1;
+	while (left < right) {
+		let mid = Math.floor((left + right) / 2);
+		if (nums[mid] > nums[mid + 1]) right = mid;
+		else left = mid + 1;
+	}
+	return left;
 };
 // @lc code=end
 
