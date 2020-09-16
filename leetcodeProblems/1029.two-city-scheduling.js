203bab13f2b43d8ad10146e5fd139ad70af0c910
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-09-16 21:38:13 +0100

    Adding 2 problems

---
+++ leetcodeProblems/1029.two-city-scheduling.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1029 lang=javascript
+ *
+ * [1029] Two City Scheduling
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+var twoCitySchedCost = function(costs) {
+    
+};
+// @lc code=end
+
---
+++ leetcodeProblems/1169.invalid-transactions.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=1169 lang=javascript
+ *
+ * [1169] Invalid Transactions
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} transactions
+ * @return {string[]}
+ */
+var invalidTransactions = function(transactions) {
+    
+};
+// @lc code=end
+
