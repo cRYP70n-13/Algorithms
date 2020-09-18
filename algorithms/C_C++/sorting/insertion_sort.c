0a783f43c5cf87d7e8fe6e9ed6f550fb3e12c4c5
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-09-18 15:21:27 +0100

    Some changes on the searching and sorting algorithms

--- algorithms/C_C++/sorting/insertion_sort.c
+++ algorithms/C_C++/sorting/insertion_sort.c
@@ -8,10 +8,9 @@ void    insertionSort(int arr[], int n)
     for (i = 1; i < n; i++) {
         key = arr[i];
         j = i - 1;
-
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
-            j -= 1;
+            j--;
         }
         arr[j + 1] = key;
     }
