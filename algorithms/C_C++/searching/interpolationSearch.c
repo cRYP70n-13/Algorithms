74c6243df66694aa2d381f700544b54dbbb095ee
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-09-19 07:07:28 +0100

    InterpolationSearch in C

--- algorithms/C_C++/searching/interpolationSearch.c
+++ algorithms/C_C++/searching/interpolationSearch.c
@@ -11,21 +11,15 @@ int interpolationSearch(int arr[], int n, int x)
     // in array must be in range defined by corner
     while (lo <= hi && x >= arr[lo] && x <= arr[hi])
     {
-        if (lo == hi){
+        if (lo == hi) {
             if (arr[lo] == x) return lo;
             return -1;
         }
-        // Probing the position with keeping
-        // uniform distribution in mind.
-        int pos = lo + (((double)(hi-lo) /
-              (arr[hi]-arr[lo]))*(x - arr[lo]));
-
+        int pos = lo + (((double)(hi-lo) / (arr[hi]-arr[lo]))*(x - arr[lo]));
         if (arr[pos] == x)
             return pos;
-
         if (arr[pos] < x)
             lo = pos + 1;
-
         else
             hi = pos - 1;
     }
@@ -34,9 +28,8 @@ int interpolationSearch(int arr[], int n, int x)
 
 int main()
 {
-    int arr[] =  {10, 12, 13, 16, 18, 19, 20, 21, 22, 23,
-                  24, 33, 35, 42, 47};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[] =  {10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42, 47};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
     int x = 18;
     int index = interpolationSearch(arr, n, x);
