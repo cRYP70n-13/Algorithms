31758e7e8847637fec3e3a8fa32acf7732b02701
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-09-19 15:09:01 +0100

    Adding the Depth first traversal for Binary Tree

---
+++ Data_Structures/C_C++/BinaryTree/preOrderTraversal.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    char data;
+    Node *left;
+    Node *right;
+};
+
+void    preOrder(Node *root)
+{
+    if (!root) return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+// Function to Insert Node in a Binary Search Tree
+Node* Insert(Node *root,char data) {
+    if (!root) {
+	root = new Node();
+	root->data = data;
+	root->left = root->right = NULL;
+    }
+    else if(data <= root->data)
+	root->left = Insert(root->left,data);
+    else
+	root->right = Insert(root->right,data);
+    return root;
+}
+
+int main() {
+    /*Code To Test the logic
+      Creating an example tree
+      M
+      / \
+      B   Q
+      / \   \
+      A   C   Z
+      */
+    Node *root = NULL;
+    root = Insert(root,'M'); root = Insert(root,'B');
+    root = Insert(root,'Q'); root = Insert(root,'Z');
+    root = Insert(root,'A'); root = Insert(root,'C');
+    //Print Nodes in Preorder.
+    cout<<"Preorder: ";
+    preOrder(root);
+    cout<<"\n";
+}
