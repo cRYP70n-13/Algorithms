c7d44bc3ead25afe9e0bc470bfa9ae1c92f4dcd5
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-09-20 22:57:10 +0100

    Adding the challenges of CSES website

---
+++ challenges/repetition.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int     main(void)
+{
+    string s; cin >> s;
+    for (int i = 0; i < s.size(); i++)
+
+    return (0);
+}
