c7fc2bcc42bd656e21129bb8764c0524082f8ada
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-10-01 23:04:57 +0100

    formating my code and my folders

--- mock_easy_questions/containsDuplicates.js
+++
@@ -1,5 +0,0 @@
-const containsDups = head => {
-    const unique = new Set(nums);
-    if(unique.size === nums.length) return false;
-    return true;
-}
--- mock_easy_questions/reverseString.js
+++
@@ -1,3 +0,0 @@
-const reverseString = s => {
-    return s.reverse();
-}
