37a172a5948c3fa2f362ba27fcc4c5ff46e2b72a
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-10-03 16:00:41 +0100

    Adding some new problems

--- leetcodeProblems/118.pascals-triangle.js
+++ leetcodeProblems/118.pascals-triangle.js
@@ -10,7 +10,16 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    
+	let res = [];
+	for (let i = 0; i < numRows; i++) {
+		res[i] = [];
+		res[i][0] = res[i][i] = 1;
+
+		for (let j = 1; j < i; j++)
+			res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+	}
+	return res;
 };
 // @lc code=end
 
+console.log(generate(5));
--- leetcodeProblems/268.missing-number.js
+++ leetcodeProblems/268.missing-number.js
@@ -9,11 +9,35 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
-	const arr = new Array(nums.length + 1).fill(-1);
-	for (let i of nums)
-		arr[i] = i;
-	return arr.indexOf(-1);
-};
+// var missingNumber = function(nums) {
+// 	const arr = new Array(nums.length + 1).fill(-1);
+// 	for (let i of nums)
+// 		arr[i] = i;
+// 	return arr.indexOf(-1);
+// };
 // @lc code=end
 
+const missingNumber = nums => {
+	const mySet = new Set(nums);
+	for (let i = 0; i < nums.length; i++)
+		if (!mySet.has(i))
+			return i;
+}
+
+/**
+ * Also in the solution area I found a very good aproach
+ * Which uses the guass formula to calculate the sum
+ * of the list in constant time and return the differnce between
+ * it and the actual sum of the list
+ */
+
+const usingGauss = nums => {
+	let expectedSum = nums.length * (nums.length + 1) / 2;
+	let sum = 0;
+	for (let i = 0; i < nums.length; i++)
+		sum += nums[i];
+	return expectedSum - sum;
+}
+
+
+console.log(usingGauss([3, 0, 1]));
