a672004808dca3022c014f4832f62cc71a5ed483
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-10-06 22:54:29 +0100

    Adding a BST problem

--- algoexpert/easy/javascript/Depth_first_search.js
+++ algoexpert/easy/javascript/Depth_first_search.js
@@ -1,3 +1,18 @@
-const depthFirstSearch = (root) => {
-	
+class Node{
+	constructor(this, name) {
+		this.children = [];
+		this.name = name;
+	}
+
+	add_child = (this, child) => {
+		this.children.push(Node(child));
+		return this;
+	}
+
+	depth_first_search = (this, array) => {
+		array.push(this.name);
+		for (const child of children)
+			child.depth_first_search(array);
+		return array;
+	}
 }
--- algoexpert/easy/python/easy.py
+++ algoexpert/easy/python/easy.py
@@ -1,86 +1,86 @@
 def twoNumSum(nums, target):
-	nums.sort()
-	left = 0
-	right = len(nums) - 1
-	while left < right:
-		currentSum = nums[left] + nums[right]
-		if currentSum == target:
-			return [nums[left], nums[right]]
-		elif currentSum > target:
-			right -= 1
-		elif currentSum < target:
-			left += 1
-	return []
+    nums.sort()
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        currentSum = nums[left] + nums[right]
+        if currentSum == target:
+            return [nums[left], nums[right]]
+        elif currentSum > target:
+            right -= 1
+        elif currentSum < target:
+            left += 1
+    return []
 
 
 def binary_search(arr, target):
-	low = 0
-	high = len(arr)
-	while low < high:
-		mid = (low + high) // 2
-		if arr[mid] == target:
-			return mid
-		elif arr[mid] > target:
-			high = mid - 1
-		elif arr[mid] < target:
-			low = mid
-	return - 1
+    low = 0
+    high = len(arr)
+    while low < high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            high = mid - 1
+        elif arr[mid] < target:
+            low = mid
+    return - 1
 
 
 def bubble_sort(arr):
-	for i in range(len(arr)):
-		for j in range(i, len(arr)):
-			if arr[i] < arr[j]:
-				tmp = arr[i]
-				arr[i] = arr[j]
-				arr[j] = tmp
-	return arr
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            if arr[i] < arr[j]:
+                tmp = arr[i]
+                arr[i] = arr[j]
+                arr[j] = tmp
+    return arr
 
 
 def find_three_largest_elements(arr):
-	threeLargestNum = [None, None, None]
-	for i in arr:
-		_findLargest(threeLargestNum, i)
-	return threeLargestNum
+    threeLargestNum = [None, None, None]
+    for i in arr:
+        _findLargest(threeLargestNum, i)
+    return threeLargestNum
 
 
 def _findLargest(threeLargestNum, num):
-	if threeLargestNum[2] is None or num > threeLargestNum[2]:
-		shiftAndUpdate(threeLargestNum, 2, num)
-	elif threeLargestNum[1] is None or num > threeLargestNum[1]:
-		shiftAndUpdate(threeLargestNum, 1, num)
-	elif threeLargestNum[0] is None or num > threeLargestNum[0]:
-		shiftAndUpdate(threeLargestNum, 0, num)
+    if threeLargestNum[2] is None or num > threeLargestNum[2]:
+        shiftAndUpdate(threeLargestNum, 2, num)
+    elif threeLargestNum[1] is None or num > threeLargestNum[1]:
+        shiftAndUpdate(threeLargestNum, 1, num)
+    elif threeLargestNum[0] is None or num > threeLargestNum[0]:
+        shiftAndUpdate(threeLargestNum, 0, num)
 
 
 def shiftAndUpdate(arr, index, num):
-	for i in range(index + 1):
-		if i == index:
-			arr[i] = num
-		else:
-			arr[i] = arr[i + 1]
+    for i in range(index + 1):
+        if i == index:
+            arr[i] = num
+        else:
+            arr[i] = arr[i + 1]
 
 
 def Nth_fibonacci_number(n):
-	if n == 1:
-		return 0
-	elif n == 2:
-		return 1
-	return Nth_fibonacci_number(n - 1) + Nth_fibonacci_number(n - 2)
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    return Nth_fibonacci_number(n - 1) + Nth_fibonacci_number(n - 2)
 
 
 def validate_subsequence(arr, subsequence):
-	j = 0
-	for i in range(len(arr)):
-		if arr[i] == subsequence[j]:
-			j += 1
-	return j == len(subsequence)
+    j = 0
+    for i in range(len(arr)):
+        if arr[i] == subsequence[j]:
+            j += 1
+    return j == len(subsequence)
 
 
-def Node_Depth(root, depth = 0):
-	if root is None:
-		return
-	return depth + Node_Depth(root.left, depth + 1) + Node_Depth(root.right, depth + 1)
+def Node_Depth(root, depth=0):
+    if root is None:
+        return
+    return depth + Node_Depth(root.left, depth + 1) + Node_Depth(root.right, depth + 1)
 
 
 def inserttion_sort(array):
@@ -93,7 +93,7 @@ def inserttion_sort(array):
 
 
 def swap(i, j, array):
-	array[i], array[j] = array[j], array[i]
+    array[i], array[j] = array[j], array[i]
 
 
 def selection_sort(array):
@@ -109,22 +109,22 @@ def selection_sort(array):
 
 
 def palindrome_check(str):
-	left = 0
-	right = len(str)
-	while left < right:
-		if str[left] != str[right]:
-			return False
-	return True
+    left = 0
+    right = len(str)
+    while left < right:
+        if str[left] != str[right]:
+            return False
+    return True
 
 
 def product_sum(arr, coef=1):
-	sum = 0
-	for elem in arr:
-		if type(elem) is list:
-			product_sum(elem, coef + 1)
-		else:
-			sum += elem
-	return sum * coef
+    sum = 0
+    for elem in arr:
+        if type(elem) is list:
+            product_sum(elem, coef + 1)
+        else:
+            sum += elem
+    return sum * coef
 
 
 def caesarCipherEncryptor(string, key):
@@ -141,45 +141,81 @@ def get_new_letter(letter, key):
 
 
 class Node:
-	def __init__(self, value):
-		self.value = value
-		self.left = None
-		self.right = None
-	
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 
 def branch_sum(root):
-	sums = []
-	_calculateBranchSum(root, 0, sums)
-	return sums
+    sums = []
+    _calculateBranchSum(root, 0, sums)
+    return sums
 
 
 def _calculateBranchSum(node, runningSum, sums):
-	if node is None:
-		return
-	newRunningSum = runningSum + node.value
-	if node.left is None and node.right is None:
-		sums.append(newRunningSum)
-		return
-	return _calculateBranchSum(node.left, newRunningSum, sums)
-	return _calculateBranchSum(node.right, newRunningSum, sums)
+    if node is None:
+        return
+    newRunningSum = runningSum + node.value
+    if node.left is None and node.right is None:
+        sums.append(newRunningSum)
+        return
+    return _calculateBranchSum(node.left, newRunningSum, sums)
+    return _calculateBranchSum(node.right, newRunningSum, sums)
 
 
 # here we have also the depth first search
 class Node:
-	def __init__(self, name):
-		self.children = []
-		self.name = name
+    def __init__(self, name):
+        self.children = []
+        self.name = name
 
-	def addChild(self, child):
-		self.chilren.append(child)
+    def addChild(self, child):
+        self.chilren.append(child)
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in children:
+            child.depthFirstSearch(array)
+        return array
+
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+	def insert(self, value):
+		if (value < self.value):
+			if self.left is None:
+				self.left = BST(value)
+			else:
+				self.left.insert(value)
+		else:
+			if self.right is None:
+				self.right = BST(value)
+			else:
+				self.right.insert(value)
 		return self
-	
-	def depthFirstSearch(self, array):
-		array.append(self.name)
-		for child in children:
-			child.depthFirstSearch(array)
-		return array
 
+
+def findClosestValue(tree, target):
+    return _findClosestBSTValueHelper(tree, target, float("inf"))
+
+
+def _findClosestBSTValueHelper(tree, target, closest):
+    if tree is None:
+        return closest
+    if abs(target - closest) > abs(tree.value - target):
+        closest = tree.value
+    if target < tree.value:
+        return _findClosestBSTValueHelper(tree.left, target, closest)
+    elif target > tree.value:
+        return _findClosestBSTValueHelper(tree.right, target, closest)
+    else:
+        return closest
 
 
 print(validate_subsequence([1, 10, 19, 24, 29, 12, 90], [1, 29, 90]))
