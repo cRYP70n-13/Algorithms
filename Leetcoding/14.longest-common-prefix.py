7b9e7e972da85f3799f6e6bab85110e4651f55b9
cRYP70n-13 <otmane.kimdil@gmail.com>
2020-11-02 20:05:36 +0100

    2 New problems in Python

---
+++ Leetcoding/14.longest-common-prefix.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=14 lang=python3
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pass
+    # At the moment I just wanna test my workspace so it's working tomorrow it's time for the problems
+        
+# @lc code=end
+
